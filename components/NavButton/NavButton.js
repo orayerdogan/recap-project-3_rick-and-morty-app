85409e92306a63f627d27b1391d1025ed39d9f41
Oray92 <oray.erdogan92@gmail.com>
2026-02-20 14:31:58 +0100

    first 3 tasks

---
+++ components/NavButton/NavButton.js
@@ -0,0 +1,6 @@
+export function setupButtons(
+  prevButton,
+  nextButton,
+  pageState,
+  fetchCharacters,
+)
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+import { createCharacterCard } from "./components/CharacterCard/CharacterCard.js";
+//import { setupButtons } from "./components/NavButton/NavButton.js";
+//import { updatePaginationDisplay } from "./components/NavPagination/NavPagination.js";
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
-  '[data-js="search-bar-container"]'
+  '[data-js="search-bar-container"]',
 );
 const searchBar = document.querySelector('[data-js="search-bar"]');
 const navigation = document.querySelector('[data-js="navigation"]');
@@ -8,7 +12,43 @@ const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
-// States
-const maxPage = 1;
-const page = 1;
+let page = 1;
+let maxPage = 1;
 const searchQuery = "";
+
+//fetchCharacters function
+async function fetchCharacters() {
+  try {
+    cardContainer.innerHTML = "";
+
+    //
+    const response = await fetch(
+      `https://rickandmortyapi.com/api/character/?page${page}`,
+    );
+    const data = await response.json();
+    maxPage = data.info.pages; //update maxPage API info
+
+    //create&append card
+    data.results.forEach((character) => {
+      const card = createCharacterCard(character);
+      cardContainer.appendChild(card);
+    });
+    pagination.textContent = `${page} / ${maxPage}`; //updates the paginations display
+  } catch (error) {
+    console.error("Error fetching characters:", error);
+  }
+}
+prevButton.addEventListener("click", () => {
+  if (page > 1) {
+    page--;
+    fetchCharacters();
+  }
+});
+
+nextButton.addEventListener("click", () => {
+  if (page < maxPage) {
+    page++;
+    fetchCharacters();
+  }
+});
+fetchCharacters();
